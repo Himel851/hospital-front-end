7563d7f714b0b70d76f59320fcfca2fba08a73e4
Nazmul Himel <himel.cse96@gmail.com>
2023-07-06 10:34:07 +0600

    appointment list

--- component/Appointment list/AppointmentList.jsx
+++ component/Appointment list/AppointmentList.jsx
@@ -1,26 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { Container, Table } from "react-bootstrap";
+import { Button, Container, Table } from "react-bootstrap";
 import { Check, X } from "react-bootstrap-icons";
 import { useAuth } from "../../context/auth";
 import { useRouter } from "next/router";
+import axios from "axios";
 
 export default function AppointmentList() {
   const [auth, setAuth] = useAuth();
   const [list, setList] = useState([]);
   const router = useRouter();
-
+  console.log(router?.query?.id)
   useEffect(() => {
     const fetchInvoiceData = async () => {
       try {
 
-        const response = await axios.get(`http://localhost:4023/api/v1/appointment/view/doctor/${auth?._id}`,
-          {
-            headers: {
-              Authorization: auth?.token,
-            }
-          }
-        );
-        setList(response?.data);
+        const response = await axios.get(`http://localhost:4023/api/v1/appointment/view/doctor/${router?.query?.id}`);
+        setList(response?.data?.data);
         console.log(response?.data)
       } catch (error) {
         // toast.error("Something went wrong");
@@ -29,6 +24,40 @@ export default function AppointmentList() {
 
     fetchInvoiceData();
   }, [router.query.id]);
+
+  const handleApprove = async (doctorId) => {
+    try {
+      const response = await axios.get(`http://localhost:4023/api/v1/admin/approve/doctor/${doctorId}`);
+      const { success, message } = response.data;
+      if (success) {
+        console.log(message);
+        // Refresh the doctor list
+        fetchDoctorList();
+        toast.success('Doctor approve successful......');
+      }
+    } catch (error) {
+      console.error('Error approving doctor:', error);
+      toast.error('Doctor approve failed......');
+    }
+  };
+
+  const handleReject = async (doctorId) => {
+    try {
+      const response = await axios.get(`http://localhost:4023/api/v1/admin/reject/doctor/${doctorId}`);
+      const { success, message } = response.data;
+      if (success) {
+        console.log(message);
+        // Refresh the doctor list
+        fetchDoctorList();
+        toast.success('Doctor Rejected successful......');
+      }
+    } catch (error) {
+      console.error('Error rejecting doctor:', error);
+      toast.error('Doctor rejected failed......');
+    }
+  };
+  
+
   return (
     <Container className="py-5" style={{ marginTop: '4rem' }}>
       <Table responsive striped bordered hover>
@@ -38,59 +67,40 @@ export default function AppointmentList() {
               <strong>Patient Name</strong>
             </th>
             <th>
-              <strong>Approve</strong>
+              <strong>Gender</strong>
             </th>
             <th>
-              <strong>Not Approve</strong>
+              <strong>Age</strong>
+            </th>
+            <th>
+              <strong>Slot</strong>
+            </th>
+            <th>
+              <strong>Reason</strong>
+            </th>
+            <th>
+              <strong>Actions</strong>
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Domain customization</td>
-            <td>
-              <Check className="text-success" />
-            </td>
-            <td>
-              <X className="text-danger" />
-            </td>
-          </tr>
-          <tr>
-            <td>FTP</td>
-            <td>
-              <Check className="text-success" />
-            </td>
-            <td>
-              <Check className="text-success" />
-            </td>
-          </tr>
-          <tr>
-            <td>Database</td>
-            <td>
-              <Check className="text-success" />
-            </td>
-            <td>
-              <X className="text-danger" />
-            </td>
-          </tr>
-          <tr>
-            <td>Support</td>
-            <td>
-              <Check className="text-success" />
-            </td>
-            <td>
-              <Check className="text-success" />
-            </td>
-          </tr>
-          <tr>
-            <td>Backups</td>
-            <td>
-              <Check className="text-success" />
-            </td>
-            <td>
-              <X className="text-danger" />
-            </td>
-          </tr>
+          {
+            list?.map((item) => (
+              <tr key={item?._id}>
+                <td>{item?.patientName}</td>
+                <td>{item?.patientGender}</td>
+                <td>{item?.patientAge}</td>
+                <td>{item?.slot}</td>
+                <td>{item?.reason}</td>
+                <td>
+                <div className='d-flex gap-2'>
+                  <Button variant="success" onClick={() => handleApprove(doctor._id)}>Approve</Button>
+                  <Button variant="danger" onClick={() => handleReject(doctor._id)}>Reject</Button>
+                </div>
+              </td>
+              </tr>
+            ))
+          }
         </tbody>
       </Table>
     </Container>
