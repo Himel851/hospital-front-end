436650f1e1281ea42bde521d2e3be51bd9cab87b
Nazmul Himel <himel.cse96@gmail.com>
2023-07-06 11:34:56 +0600

    change

--- component/Appointment list/AppointmentList.jsx
+++ component/Appointment list/AppointmentList.jsx
@@ -9,14 +9,15 @@ export default function AppointmentList() {
   const [auth, setAuth] = useAuth();
   const [list, setList] = useState([]);
   const router = useRouter();
-  console.log(router?.query?.id)
+  console.log(router?.query?.id);
   useEffect(() => {
     const fetchInvoiceData = async () => {
       try {
-
-        const response = await axios.get(`http://localhost:4023/api/v1/appointment/view/doctor/${router?.query?.id}`);
+        const response = await axios.get(
+          `http://localhost:4023/api/v1/appointment/view/doctor/${router?.query?.id}`
+        );
         setList(response?.data?.data);
-        console.log(response?.data)
+        console.log(response?.data);
       } catch (error) {
         // toast.error("Something went wrong");
       }
@@ -27,39 +28,42 @@ export default function AppointmentList() {
 
   const handleApprove = async (doctorId) => {
     try {
-      const response = await axios.get(`http://localhost:4023/api/v1/admin/approve/doctor/${doctorId}`);
+      const response = await axios.get(
+        `http://localhost:4023/api/v1/admin/approve/doctor/${doctorId}`
+      );
       const { success, message } = response.data;
       if (success) {
         console.log(message);
         // Refresh the doctor list
         fetchDoctorList();
-        toast.success('Doctor approve successful......');
+        toast.success("Doctor approve successful......");
       }
     } catch (error) {
-      console.error('Error approving doctor:', error);
-      toast.error('Doctor approve failed......');
+      console.error("Error approving doctor:", error);
+      toast.error("Doctor approve failed......");
     }
   };
 
   const handleReject = async (doctorId) => {
     try {
-      const response = await axios.get(`http://localhost:4023/api/v1/admin/reject/doctor/${doctorId}`);
+      const response = await axios.get(
+        `http://localhost:4023/api/v1/admin/reject/doctor/${doctorId}`
+      );
       const { success, message } = response.data;
       if (success) {
         console.log(message);
         // Refresh the doctor list
         fetchDoctorList();
-        toast.success('Doctor Rejected successful......');
+        toast.success("Doctor Rejected successful......");
       }
     } catch (error) {
-      console.error('Error rejecting doctor:', error);
-      toast.error('Doctor rejected failed......');
+      console.error("Error rejecting doctor:", error);
+      toast.error("Doctor rejected failed......");
     }
   };
-  
 
   return (
-    <Container className="py-5" style={{ marginTop: '4rem' }}>
+    <Container className="py-5" style={{ marginTop: "4rem" }}>
       <Table responsive striped bordered hover>
         <thead>
           <tr>
@@ -84,27 +88,33 @@ export default function AppointmentList() {
           </tr>
         </thead>
         <tbody>
-          {
-            list?.map((item) => (
-              <tr key={item?._id}>
-                <td>{item?.patientName}</td>
-                <td>{item?.patientGender}</td>
-                <td>{item?.patientAge}</td>
-                <td>{item?.slot}</td>
-                <td>{item?.reason}</td>
-                <td>
-                <div className='d-flex gap-2'>
-                  <Button variant="success" onClick={() => handleApprove(doctor._id)}>Approve</Button>
-                  <Button variant="danger" onClick={() => handleReject(doctor._id)}>Reject</Button>
+          {list?.map((item) => (
+            <tr key={item?._id}>
+              <td>{item?.patientName}</td>
+              <td>{item?.patientGender}</td>
+              <td>{item?.patientAge}</td>
+              <td>{item?.slot}</td>
+              <td>{item?.reason}</td>
+              <td>
+                <div className="d-flex gap-2">
+                  <Button
+                    variant="success"
+                    onClick={() => handleApprove(doctor._id)}
+                  >
+                    Approve
+                  </Button>
+                  <Button
+                    variant="danger"
+                    onClick={() => handleReject(doctor._id)}
+                  >
+                    Reject
+                  </Button>
                 </div>
               </td>
-              </tr>
-            ))
-          }
+            </tr>
+          ))}
         </tbody>
       </Table>
     </Container>
   );
 }
-
-
